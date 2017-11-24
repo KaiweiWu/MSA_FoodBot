14f56be7ce8236168dd6526a79d4d30e350f9d05
Kaiwei Wu <kaiwei-wu@hotmail.com>
2017-11-24 14:30:53 +1300

    Bug fix2

--- controller/QnAMaker.js
+++ controller/QnAMaker.js
@@ -1,7 +1,7 @@
 var rest = require('../API/Restclient');
 
 exports.talkToQnA = function postQnAResults(session, question){
-    var url = 'https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/[REFER TO IMAGE ABOVE]/generateAnswer';
+    var url = 'https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/15e3e4d9-e61a-4232-b23b-eff63d40039c/generateAnswer';
     rest.postQnAResults(url, session, question, handleQnA)
 };
 
